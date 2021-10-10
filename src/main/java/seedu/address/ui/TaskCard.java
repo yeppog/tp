6d6dfe598a253063395c1d31ca043715f835d73c
Jeff Sieu <jeffsy00@gmail.com>
2021-10-10 15:37:40 +0800

    Fix checkstyle again

--- src/main/java/seedu/address/ui/TaskCard.java
+++ src/main/java/seedu/address/ui/TaskCard.java
@@ -1,12 +1,12 @@
 package seedu.address.ui;
 
+import java.util.Optional;
+
 import javafx.fxml.FXML;
 import javafx.scene.control.CheckBox;
 import javafx.scene.control.Label;
 import javafx.scene.layout.Region;
 import seedu.address.model.task.Task;
-
-import java.util.Optional;
 
 public class TaskCard extends UiPart<Region> {
     private static final String FXML = "TaskCard.fxml";
@@ -28,6 +28,11 @@ public class TaskCard extends UiPart<Region> {
 
     private final Task task;
 
+    /**
+     * Creates a card representing a task. Used in a task list to display a task.
+     * @param task The task to represent
+     * @param oneIndex The position of the task in the list in one-based indexing
+     */
     public TaskCard(Task task, int oneIndex) {
         super(FXML);
 
