174903f90bf19b59cc0c8e0647290b8d8d75fe86
Ren Weilin <66356390+wlren@users.noreply.github.com>
2021-10-11 22:27:18 +0800

    Update DeleteCommandParser.java

--- src/main/java/seedu/address/logic/parser/DeleteCommandParser.java
+++ src/main/java/seedu/address/logic/parser/DeleteCommandParser.java
@@ -12,7 +12,7 @@ import seedu.address.logic.parser.exceptions.ParseException;
 public class DeleteCommandParser implements Parser<DeleteCommand> {
 
     /**
-     * Parses the given {@code String} of arguments in the context of the DeleteTaskCommand
+     * Parses the given {@code String} of arguments in the context of the DeleteCommand
      * and returns a DeleteCommand object for execution.
      * @throws ParseException if the user input does not conform to the expected format
      */
