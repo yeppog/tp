21d522c0cb708a077db74550244f614b7f8e9fbb
Jonathan <jon.lee.jing.yu@gmail.com>
2021-10-20 22:06:57 +0800

    fix: Allow DoneCommand to set tasks to Undone

--- src/main/java/seedu/address/logic/commands/task/DoneTaskCommand.java
+++ src/main/java/seedu/address/logic/commands/task/DoneTaskCommand.java
@@ -20,10 +20,8 @@ public class DoneTaskCommand extends TaskCommand {
     public static final String FULL_COMMAND_WORD = TaskCommand.COMMAND_WORD + " " + COMMAND_WORD;
     public static final String MESSAGE_SUCCESS = "Task completed: %1$s";
     public static final String MESSAGE_ALREADY_DONE = "Task has already been completed: %1$s";
-    public static final String MESSAGE_USAGE = FULL_COMMAND_WORD
-            + ": Completes an existing task in the task list.\n"
-            + "Parameters: INDEX (must be a positive integer) "
-            + "Example: " + FULL_COMMAND_WORD + " 1";
+    public static final String MESSAGE_USAGE =
+            FULL_COMMAND_WORD + ": Completes an existing task in the task list.\n" + "Parameters: INDEX (must be a positive integer) " + "Example: " + FULL_COMMAND_WORD + " 1";
 
     private final Index index;
 
@@ -41,21 +39,27 @@ public class DoneTaskCommand extends TaskCommand {
         }
 
         Task task = taskList.get(index.getZeroBased());
-        if (task.getIsDone()) {
-            throw new CommandException(String.format(MESSAGE_ALREADY_DONE, task));
-        }
+        Task completedTask = new Task(task.getTitle(),
+                task.getDescription(),
+                task.getTimestamp(),
+                task.getTags(),
+                !task.getIsDone());
+        model.setTask(task,
+                completedTask);
 
-        Task completedTask = new Task(task.getTitle(), task.getDescription(), task.getTimestamp(),
-                task.getTags(), !task.getIsDone());
-        model.setTask(task, completedTask);
-
-        return new CommandResult(String.format(MESSAGE_SUCCESS, completedTask));
+        return new CommandResult(String.format(MESSAGE_SUCCESS,
+                completedTask));
     }
 
     @Override
     public boolean equals(Object o) {
-        return this == o
-                || (o instanceof DoneTaskCommand
-                && index.equals(((DoneTaskCommand) o).index));
+        return this == o || (o instanceof DoneTaskCommand && index.equals(((DoneTaskCommand) o).index));
+    }
+
+    @Override
+    public CommandResult undo(Model model) throws CommandException {
+        this.execute(model);
+        return new CommandResult("Changed the DONE status of " + this.index +
+                " back to its previous completion " + "status");
     }
 }
