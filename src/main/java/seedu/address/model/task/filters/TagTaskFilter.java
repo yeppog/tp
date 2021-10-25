c878dac274a0c028ba37aece084008f9b8f88bc0
Jeff Sieu <jeffsy00@gmail.com>
2021-10-25 18:29:15 +0800

    Refactor TaskFilters into subclasses
    # Conflicts:
    #	src/main/java/seedu/address/model/task/filters/TaskFilters.java

---
+++ src/main/java/seedu/address/model/task/filters/TagTaskFilter.java
@@ -0,0 +1,42 @@
+package seedu.address.model.task.filters;
+
+import seedu.address.model.tag.Tag;
+
+public class TagTaskFilter extends TaskFilter {
+    private final Tag tag;
+
+    TagTaskFilter(Tag tag) {
+        this(tag, false);
+    }
+
+    private TagTaskFilter(Tag tag, boolean isInverted) {
+        super(task -> task.getTags().contains(tag), isInverted);
+        this.tag = tag;
+    }
+
+    @Override
+    public String toDisplayString() {
+        return (isInverted ? "Not tagged " : "Tagged ") + tag;
+    }
+
+    @Override
+    public TagTaskFilter invert() {
+        return new TagTaskFilter(tag, !isInverted);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+
+        return (o instanceof TagTaskFilter)
+                && isInverted == ((TagTaskFilter) o).isInverted
+                && tag.equals(((TagTaskFilter) o).tag);
+    }
+
+    @Override
+    public int hashCode() {
+        return tag.hashCode();
+    }
+}
