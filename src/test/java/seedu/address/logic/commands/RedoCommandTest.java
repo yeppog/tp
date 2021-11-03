e3eced88675210ef9d72786195ec43bc334d8434
Koh Jia Xian <39460640+koh-jx@users.noreply.github.com>
2021-11-03 14:53:10 +0800

    Add various tests, redo test

---
+++ src/test/java/seedu/address/logic/commands/RedoCommandTest.java
@@ -0,0 +1,21 @@
+package seedu.address.logic.commands;
+
+import org.junit.jupiter.api.Test;
+import seedu.address.model.Model;
+import seedu.address.model.ModelManager;
+import seedu.address.model.UserPrefs;
+
+import static seedu.address.testutil.TypicalPersons.getTypicalAddressBook;
+import static seedu.address.testutil.TypicalTasks.getTypicalTaskList;
+
+class RedoCommandTest {
+
+    private final Model model = new ModelManager(getTypicalAddressBook(), getTypicalTaskList(), new UserPrefs());
+
+    @Test
+    void execute_invalid_undo() {
+        RedoCommand redoCommand = new RedoCommand();
+        CommandTestUtil.assertCommandSuccess(redoCommand, model, RedoCommand.MESSAGE_NOT_UNDONE, model);
+    }
+
+}
--- src/test/java/seedu/address/logic/commands/task/DeleteTaskCommandTest.java
+++ src/test/java/seedu/address/logic/commands/task/DeleteTaskCommandTest.java
@@ -9,9 +9,9 @@ import org.junit.jupiter.api.Test;
 
 import seedu.address.commons.core.Messages;
 import seedu.address.commons.core.index.Index;
-import seedu.address.model.Model;
-import seedu.address.model.ModelManager;
-import seedu.address.model.UserPrefs;
+import seedu.address.logic.commands.RedoCommand;
+import seedu.address.logic.commands.UndoCommand;
+import seedu.address.model.*;
 import seedu.address.model.tag.Tag;
 import seedu.address.model.task.Task;
 import seedu.address.model.task.filters.TaskFilters;
@@ -56,6 +56,27 @@ public class DeleteTaskCommandTest {
         DeleteTaskCommand deleteTaskCommand = new DeleteTaskCommand(targetIndex);
         assertCommandFailure(deleteTaskCommand, model, Messages.MESSAGE_INVALID_TASK_DISPLAYED_INDEX);
 
+    }
+
+    @Test
+    public void undoRedo_validIndexUnfilteredList_success() {
+        Model originalModel = new ModelManager(
+                new AddressBook(model.getAddressBook()), new TaskList(model.getTaskList()), new UserPrefs());
+
+        Index targetIndex = Index.fromOneBased(1);
+        Task taskToDelete = model.getFilteredTaskList().get(targetIndex.getZeroBased());
+        DeleteTaskCommand deleteTaskCommand = new DeleteTaskCommand(targetIndex);
+        String expectedMessage = String.format(DeleteTaskCommand.MESSAGE_SUCCESS, taskToDelete);
+        ModelManager expectedModel = ModelManager.from(model);
+        expectedModel.deleteTask(taskToDelete);
+        assertCommandSuccess(deleteTaskCommand, model, expectedMessage, expectedModel);
+        model.getCommandHistory().pushCommand(deleteTaskCommand);
+
+        String successUndoMessage = UndoCommand.MESSAGE_UNDO_SUCCESS + expectedMessage;
+        assertCommandSuccess(new UndoCommand(), model, successUndoMessage, originalModel);
+
+        String successRedoMessage = RedoCommand.MESSAGE_REDO_SUCCESS + expectedMessage;
+        assertCommandSuccess(new RedoCommand(), model, successRedoMessage, expectedModel);
     }
 
 }
